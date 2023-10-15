624ca65577dbee2bb4b29dca638c28a1f4f43fc2
Julien <julien.thoumy@gmail.com>
2023-10-15 23:21:54 +0200

    ajout API pour durée et distance possibilité de choisir les villes de départ et arrivé

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
+const key = "SQCVM9uDdJ0aC2utMPkJ2L5fj3uGn5y8";
+let from = "toulon";
+let to = "nice";
+let distanceKilometre;
+
+const directionsForm = document.getElementById('directions-form');
+
+if (directionsForm) {
+    directionsForm.addEventListener('submit', function(event) {
+        event.preventDefault(); // Empêche la soumission du formulaire
+
+        // Récupère les nouvelles valeurs des champs "Point de départ" et "Point d'arrivée"
+        const newPointDepart = document.getElementById('point-depart').value;
+        const newPointArrivee = document.getElementById('point-arrivee').value;
+
+        // Met à jour les variables from et to avec les nouvelles valeurs
+        from = newPointDepart;
+        to = newPointArrivee;
+
+        // Effectuer la requête vers MapQuest pour obtenir la distance
+        fetch(`https://www.mapquestapi.com/directions/v2/route?key=${key}&from=${from}&to=${to}&outFormat=json&ambiguities=ignore&routeType=fastest&doReverseGeocode=false&enhancedNarrative=false&avoidTimedConditions=false`)
+            .then(response => response.json())
+            .then(data => {
+                // Récupération de la distance en kilomètres à partir de la réponse de l'API
+                let distanceMiles = data.route.distance;
+                distanceKilometre = (distanceMiles * 1.60934).toFixed(2); // Affecte la valeur à la variable globale
+                let duree = data.route.formattedTime;
+
+                document.getElementById('km').textContent = `${distanceKilometre} Km`;
+                document.getElementById('time').textContent = `${duree} H`;
+
+                // Ensuite, vous pouvez appeler la fonction calculatePrice() pour effectuer le calcul en fonction des nouvelles valeurs
+                calculatePrice();
+            });
+    });
+}
+
 function calculatePrice() {
     // Récupération des valeurs des champs du formulaire
-    const distance = parseFloat(document.getElementById('distance').value);
     const consommation = parseFloat(document.getElementById('consommation').value);
     const prixCarburant = parseFloat(document.getElementById('prix-carburant').value);
     const coutPeage = parseFloat(document.getElementById('cout-peage').value);
@@ -9,7 +45,7 @@ function calculatePrice() {
     const passagers = parseInt(document.getElementById('passagers').value);
 
     // Vérification des données d'entrée (assurez-vous qu'elles sont numériques et valides)
-    if (isNaN(distance) || isNaN(consommation) || isNaN(prixCarburant) || isNaN(coutPeage) || isNaN(nombreTrajets)) {
+    if (isNaN(consommation) || isNaN(prixCarburant) || isNaN(coutPeage) || isNaN(nombreTrajets)) {
         alert("Veuillez saisir des valeurs numériques valides.");
         return;
     }
@@ -17,8 +53,8 @@ function calculatePrice() {
     // Conversion de la consommation en litres par kilomètre
     const consommationParKm = (consommation / 100);
 
-    // Calcul du coût total du carburant en supposant que le conducteur effectue la distance totale
-    let coutCarburantTotal = distance * consommationParKm * prixCarburant;
+    // Utilisez la variable distanceKilometre déjà déclarée
+    let coutCarburantTotal = distanceKilometre * consommationParKm * prixCarburant;
 
     // Prise en compte des frais de péage pour l'aller-retour
     if (allerRetour) {
@@ -28,20 +64,23 @@ function calculatePrice() {
         coutCarburantTotal += coutPeage; // Les frais de péage pour un seul trajet
     }
 
+    // Estimation du coût d'usure de la voiture
+    const coutUsureParKm = 1500 / 15000; // 1500 € par an pour 15 000 km
+    const coutUsureTotal = distanceKilometre * coutUsureParKm;
+
     // Calcul du coût total du trajet en fonction du nombre de trajets
-    const coutTotalTrajet = coutCarburantTotal * nombreTrajets;
+    const coutTotalTrajet = (coutCarburantTotal * nombreTrajets + coutUsureTotal).toFixed(2);
 
     // Affichage du coût total du trajet
-    document.getElementById('resultat-prix').textContent = `Coût total du trajet : ${coutTotalTrajet.toFixed(2)} €`;
+    const resultatPrix = document.getElementById('resultat-prix');
+    resultatPrix.textContent = `Coût total du trajet : ${coutTotalTrajet} €`;
 
     // Calcul du coût par passager en fonction du nombre de passagers
+    const prixPassagers = document.getElementById('prix-passagers');
     if (passagers > 0) {
-        const coutParPassager = coutTotalTrajet / (passagers + 1); // +1 pour inclure le conducteur
-        document.getElementById('prix-passagers').textContent = `Coût par passager : ${coutParPassager.toFixed(2)} € par passager`;
+        const coutParPassager = (coutTotalTrajet / (passagers + 1)).toFixed(2);
+        prixPassagers.textContent = `Coût par passager : ${coutParPassager} € par passager`;
     } else {
-        document.getElementById('prix-passagers').textContent = `Coût par passager : 0.00 €`;
+        prixPassagers.textContent = `Coût par passager : 0.00 €`;
     }
 }
-
-// Écouteur d'événement pour le changement du nombre de passagers
-document.getElementById('passagers').addEventListener('change', calculatePrice);
